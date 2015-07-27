2b6ba69ace27b22d2a9f4ccf9c929bf83d8a0502
Abdallah <abdallaharar@turnkii.com>
2015-07-27 16:08:40 -0400

    set proper datatypes for numerical fields and added a number of review field to the model

--- db/migrate/20150724200912_create_electricians.rb
+++ db/migrate/20150724200912_create_electricians.rb
@@ -2,31 +2,31 @@ class CreateElectricians < ActiveRecord::Migration
   def change
     create_table :electricians do |t|
       t.string :name
-      t.string :phone
+      t.integer :phone, :limit => 8
       t.string :location
       t.string :website
-      t.string :score 
+      t.decimal :score
+      t.integer :num_reviews 
       t.string :products
       t.string :services
       t.string :brands
       t.string :styles
       t.string :specialties
       t.string :year
-      t.string :num_of_emp
+      t.integer :num_of_emp
       t.string :return_pol
       t.string :payment
       t.string :licenses
       t.string :membership
-      t.boolean :liability
-      t.boolean :worker_comp
-      t.boolean :bonded
-      t.boolean :contract
+      t.integer :liability, :limit => 1
+      t.integer :worker_comp, :limit => 1
+      t.integer :bonded, :limit => 1
+      t.integer :contract, :limit => 1  
       t.string :warrenty
       t.string :diplomas
       t.string :project_min
       t.string :project_rate
       t.string :company_profile
-
       t.timestamps null: false
     end
   end
